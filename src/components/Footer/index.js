5ef2bfcd8b83927dbf83a2c64eb4312bc7a441d8
tbellenger <tbellenger@gmail.com>
2022-01-23 15:45:26 -0800

    initial commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      <div className='container'>
+        <div className='content has-text-centered'>Footer Text Here</div>
+      </div>
+    </footer>
+  );
+}
