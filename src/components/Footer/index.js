b1d747a5f4529c8f8bcedc7934bfcbb6cf560a89
tbellenger <tbellenger@gmail.com>
2022-02-02 08:58:25 -0800

    added footer elements and links to resume and skills

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,24 @@
 import React from "react";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <footer className='footer mt-auto pt-3 bg-light text-center text-lg-start'>
       <div
         className='text-center p-2'
-        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+        style={{ fontSize: "12px", backgroundColor: "rgba(0, 0, 0, 0.2)" }}
       >
-        © 2020 Copyright:&nbsp;
+        © 2020 Copyright:&nbsp;Tom Bellenger&nbsp;
         <a className='text-dark' href='mailto:tbellenger@gmail.com'>
-          Tom Bellenger
+          <FaEnvelope />
+        </a>
+        &nbsp;&nbsp;
+        <a className='text-dark' href='https://github.com/tbellenger'>
+          <FaGithub />
+        </a>
+        &nbsp;&nbsp;
+        <a className='text-dark' href='https://linkedin.com/in/tbellenger'>
+          <FaLinkedin />
         </a>
       </div>
     </footer>
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaFilePdf } from "react-icons/fa";
 
 export default function Resume() {
   return (
@@ -6,6 +7,14 @@ export default function Resume() {
       <span className='gutter col-sm-12 col-md-2'></span>
       <div className='section-cta card mt-1 col-sm-12 col-md-8'>
         <div className='resume card-panel pt-1'>
+          <a href={require("../../assets/docs/CV-JUN-2021.pdf")}>
+            Resume <FaFilePdf size={28} />
+          </a>
+          <p>
+            Expertise in: Mobile Banking, FinTech, Android, Apple Pay, Google
+            Pay, Samsung Pay, EMV, NFC, Contactless Payments, Product
+            Development, Payments, Identity
+          </p>
           <iframe
             title='Resume'
             src={require("../../assets/docs/CV-JUN-2021.pdf")}
